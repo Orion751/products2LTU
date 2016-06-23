5266527d8acf40e632e2a750e7b122e2f1c3c611
Orion751 <Orion751@users.noreply.github.com>
2016-06-23 01:22:22 -0400

    Added products2LTU.sh

---
+++ products2LTU.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Usage: newScript creds source
+
+: '
+
+Pseudocode
+
+    tag1Key = source
+    tag1Value = bestbuy
+    tag2Key = producttype
+
+For every dir
+
+    tag2Value = dir
+
+    for every subdir in dir
+
+    make new visual with name from subdir
+
+    for every image in subdir
+
+        add the image to the new visual
+
+'
+
+creds="$1"
+
+source="$2"
+
+tag1Key='source'
+tag1Value='Best Buy'
+tag2Key='product type'
+
+tag2Value=''
+
+for dir in "$source"/*; do
+    tag2Value=`basename "$dir"`
+
+      for subDir in "$dir"/*; do
+          
+          visName=`basename "$subDir"`
+          visID=`curl -i -X POST -u "$creds" https://cloud.ltutech.com/api/v1/projects/491/visuals/ -F title="$visName" -F name="$visName" -F metadata-0-key="$tag1Key" -F metadata-0-value="$tag1Value" -F metadata-1-key="$tag2Key" -F metadata-1-value="$tag2Value" | tail -1 | python -mjson.tool | grep \"id\" | head -1 | grep -oP '\d*'`
+
+          for image in "$subDir"/*; do
+
+              curl -i -X POST -u "$creds" https://cloud.ltutech.com/api/v1/projects/visuals/"$visID"/images/ -F image=@"$image"
+
+          done
+
+      done
+
+done
+
